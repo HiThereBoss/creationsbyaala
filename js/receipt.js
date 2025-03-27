f973ed0c2017a4024722177107b55f447ec5af55
tiyajathan <jathant@mcmaster.ca>
2025-03-27 17:44:56 -0400

    receipt

---
+++ js/receipt.js
@@ -0,0 +1,10 @@
+const items = [
+    { id: 1, name: 'Item1', price: 10.00 },
+    { id: 2, name: 'Item2', price: 20.00 },
+    { id: 3, name: 'Item3', price: 50.25 },
+    { id: 4, name: 'Item4', price: 75.00 },
+    { id: 5, name: 'Item5', price: 100.00 },
+]
+
+const taxRate = 0.10
+
