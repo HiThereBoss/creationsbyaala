85fab86d976888a44740375147c66953f24a91e7
sr3yo <sawsib560@gmail.com>
2025-04-03 01:34:57 -0400

    ADDED PHP CODE TO GETPRODUCT

--- categories/getProduct.php
+++ categories/getProduct.php
@@ -1,5 +1,25 @@
 <?php
 
     include '../../connect.php';
+    $productId = filter_input(INPUT_GET, 'productId', FILTER_VALIDATE_INT);
+
+    if($productId) {
+        $query = "SELECT * FROM products WHERE id = :productId";
+        $stmt = $dbh->prepare($query);
+        $stmt->bindParam(':productId', $productId);
+        $stmt->execute();
+        
+        $product = $stmt->fetch();
+        
+        if ($product) {
+            echo json_encode($product);
+        } else {
+            echo json_encode(['error' => 'Product not found']);
+        }
+    } else {
+        echo json_encode(['error' => 'Invalid product ID']);
+    }
+
+
     
 ?>
